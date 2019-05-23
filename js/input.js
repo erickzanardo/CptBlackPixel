804d74338f4d38521005515e906c20f79efe5492
Erick Zanardo <erickzanardoo@gmail.com>
2019-05-23 16:54:53 -0300

    Adding touch controls

--- js/input.js
+++ js/input.js
@@ -26,6 +26,26 @@
           onStopMoving()
         }
       })
+
+      const gameCanvas = document.querySelector("#game")
+      const middleOfScreen = gameCanvas.width / 2
+      gameCanvas.addEventListener("touchstart", evt => {
+        const touch = evt.targetTouches[0]
+
+        if (touch.screenX >= middleOfScreen) {
+          onMoveRight()
+        } else {
+          onMoveLeft()
+        }
+
+        evt.preventDefault && evt.preventDefault();
+      })
+
+      gameCanvas.addEventListener("touchend", evt => {
+        onStopMoving()
+
+        evt.preventDefault && evt.preventDefault();
+      })
     }
   }
 
